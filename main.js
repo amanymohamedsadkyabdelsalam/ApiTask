43876e3f76c0651bafd4f119b88ab8c522f45502
Amany Mohamed <amanymohamedop0.gmail.com>
2025-12-06 21:11:33 +0200

    intial

---
+++ main.js
@@ -0,0 +1,36 @@
+async function fetchData() {
+  const respond = await fetch("https://facestoreapi.com/products");
+  if (!respond.ok) {
+    throw new Error("API Failed!");
+  }
+  const data = await respond.json();
+  return data;
+}
+
+let di = document.getElementById("products");
+
+async function displayProducts() {
+  try {
+    let product = await fetchData();
+    di.innerHTML = "";
+    product.forEach((element) => {
+      di.innerHTML += `
+        <div class="prod-card">
+           <h3 class="ti">${element.title}</h3>
+           <div class="img">
+              <img src="${element.image}" alt="product ${element.id}" id="i">
+           </div> 
+           <p class="disc">${element.description}</p>
+           <p class="catg"><span>Category : </span>${element.category}</p>
+           <p class="catg"><span>Price : </span> $ ${element.price}</p>
+           <p class="catg"><span>Rating : </span>${element.rating.rate} (based on ${element.rating.count} reviews)</p>
+        </div>
+       `;
+    });
+  } catch (error) {
+    di.innerHTML = `<p>Failed to load products </p>`;
+    console.error(error);
+  }
+}
+
+displayProducts();
